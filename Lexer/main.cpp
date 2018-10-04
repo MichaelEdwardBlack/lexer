20e39644ca3ed1cbf91f010e44ef4a84b2cbf73b
Michael Black <41597972+michaelblack117@users.noreply.github.com>
2018-10-04 16:53:03 -0600

    Update main.cpp
    Final main for project 1. Passed all tests

--- Lexer/main.cpp
+++ Lexer/main.cpp
@@ -8,15 +8,16 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	//OPEN FILES
 	ifstream in(argv[1]);
-	ofstream out(argv[2]);
+	//ofstream out(argv[2]); //use this if you want to output to a file
 
 	Lexer lex;
 	lex.scan(in);
 
-	out << lex.printTokens() << "Total Tokens = " << lex.getNumTokens() << endl;
+	cout << lex.printTokens() << "Total Tokens = " << lex.getNumTokens(); 
+	//took away endl because our professor doesn't want it there, even though that is standard practice
 
 	in.close();
-	out.close();
+	//out.close(); //if using output file remember to change cout to out
 
 	return 0;
 }
